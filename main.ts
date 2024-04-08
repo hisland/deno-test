ca3db43de6b91c7ea2fd56b1271a6448d62f875f
29027-何定良 <dingliang.he@hand-china.com>
2024-04-08 23:25:38 +0800

    update

--- main.ts
+++ main.ts
@@ -6,4 +6,6 @@ export function add(a: number, b: number): number {
 if (import.meta.main) {
   console.log('Add 2 + 3 =', add(2, 3))
   console.log('Add 2 + 3 =', add(2, 3))
+  console.log('Add 2 + 3 =', add(2, 3))
+  console.log('Add 2 + 3 =', add(2, 3))
 }
